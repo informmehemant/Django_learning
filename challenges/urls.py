715e686dff411ff5443fb44829dbc1df90696407
callmehemant <hemant.singh@standardbank.co.za>
2025-03-15 13:11:45 +0530

    templates and rendering

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # path('february', views.february),
     # path('march', views.march),
     # path('april', views.april),
-    path('', views.index),
+    path('', views.index, name="index"),
     path('<int:month>', views.monthly_challenges_by_num),
     path('<str:month>', views.monthly_challenge, name="month-challenge")
 
